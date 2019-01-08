bbb71546f02d40cde756af04a69c7c7a1025c586
golkarb <bgolkar.work@gmail.com>
2019-01-07 23:28:40 -0800

    updated learn1.py with sets

--- learn1.py
+++ learn1.py
@@ -3,8 +3,10 @@ print("Hello World!")
 name = "micheal jackson"
 print("before upper:", name, "\nafter upper:", name.upper(),"\n")
 
+print("===================================\n")
+
 tuple1=("disc", 10, (0, 1))
-print("example of a tuple:", tuple1)
+print("example of a tuple (idenitified with round brackets):", tuple1)
 print("last element of the example tuple is:", tuple1[-1])
 print("the first two elements of the tuple are", tuple1[0:2],"\n")
 
@@ -19,8 +21,10 @@ tuple3=(0, -2, 10)
 print("3rd tuple example:", tuple3)
 print("sorted tuple generate a list (with square brackets instead of round brackets):", sorted(tuple3),"\n")
 
+print("===================================\n")
+
 list1=["Michael", (0, 1), [4, 5]]
-print("1st list example:", list1)
+print("1st list example (identified with square brackets):", list1)
 list1.extend(['pop', 23])
 print("extending with list ['pop' and 23]:", list1)
 list1=["Michael", (0, 1), [4, 5]]
@@ -41,4 +45,26 @@ print("list2 changes as the first element of list3 changes to 'naranja':", list2
 list4=list2[:]
 print("list4 is a clone of list2:", list4)
 list4[0]='manzana'
-print("list2 does not change as the first element of list4 changes back to 'manzana'':", list2)
+print("list2 does not change as the first element of list4 changes back to 'manzana'':", list2, "\n")
+
+print("===================================\n")
+
+dict1={"key1": 0, "key2": "John", 0: "Mary", "key3": 5}
+print("1st dictionary (identified with swiggly brackets):", dict1)
+print("keys of the 1st dictionary:", dict1.keys())
+del(dict1[0])
+print("item with key:0 has been removed:", dict1)
+
+set1={"pop", "rock", "soul", "hard rock", "rock", "R&B", "rock", "disco"}
+print("1st set (identified with swiggly brackets but no keys)", set1)
+list5=[1, 2, 3, 4, 5, 5, 1, 2, 3]
+print("a list can be converted into a set (but with no duplicates):", set(list5))
+set2=set(list5)
+set2.add(6)
+set2.remove(1)
+print("elemets can be added/removed to/from a set:", set2, "\n")
+
+set3={1,2,3,4,5}
+set4={3,4,5,6,7}
+print("set3:", set3, "set4", set4)
+print("union:", set3.union(set4), "difference:", set3.difference(set4), "intersection:", set3.intersection(set4))
